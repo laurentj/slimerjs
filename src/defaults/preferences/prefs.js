0535a4264292aa56202235001873e6ffa96704d3
Laurent Jouanneau <ljouanneau@gmail.com>
2013-01-02 09:37:55 +0100

    add some prefs...

--- src/defaults/preferences/prefs.js
+++ src/defaults/preferences/prefs.js
@@ -2,6 +2,9 @@
 pref("toolkit.defaultChromeURI", "chrome://slimerjs/content/slimerjs.xul");
 pref("browser.dom.window.dump.enabled", true);
 pref("javascript.options.strict", true);
+pref("browser.cache.disk.enable", false);
+pref("app.update.enable", false);
+pref("extensions.update.enable", false);
 
 // remove it for releases
 pref("nglayout.debug.disable_xul_cache", true);
