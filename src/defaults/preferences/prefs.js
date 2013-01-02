284b28c1a039c69401ee7aa9aaf5351051fbf1d1
Laurent Jouanneau <laurent@jelix.org>
2013-01-02 11:35:27 +0100

    Fix bug: the provided js script was not reloaded after successive executions of slimerJS
    The startupcache directory of the mozilla profile should be cleared at startup.
    Note for later: we could have an option to keep cache. It could be useful in production environment.

--- src/defaults/preferences/prefs.js
+++ src/defaults/preferences/prefs.js
@@ -5,6 +5,7 @@ pref("javascript.options.strict", true);
 pref("browser.cache.disk.enable", false);
 pref("app.update.enable", false);
 pref("extensions.update.enable", false);
+pref("dom.report_all_js_exceptions", true);
 
 // remove it for releases
 pref("nglayout.debug.disable_xul_cache", true);
