a152ec5af0080b5749095f837efc997767bf97e0
Laurent Jouanneau <ljouanneau@gmail.com>
2013-03-14 10:01:27 +0100

    Open a webpage in its own window. part #1

---
+++ src/chrome/slimerjs/content/webpage.js
@@ -0,0 +1,17 @@
+
+
+window.addEventListener("load", function(event){
+
+    let browser = document.createElement("webpage");
+    function onReady(event) {
+        browser.removeEventListener("BrowserReady", onReady, false);
+        if ("arguments" in window
+            && window.arguments[0]
+            && window.arguments[0].callback) {
+            window.arguments[0].callback(browser);
+        }
+    }
+    browser.addEventListener("BrowserReady", onReady, false);
+    document.documentElement.appendChild(browser);
+
+}, true);
