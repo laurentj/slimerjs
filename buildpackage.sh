5f346bdf341d76a96747d0204a2f18a78f0aebde
Laurent Jouanneau <ljouanneau@gmail.com>
2013-04-20 11:40:31 +0200

    Improved the build script

--- buildpackage.sh
+++ buildpackage.sh
@@ -1,27 +1,46 @@
 #!/bin/bash
 
-CURRENTDIR=`dirname $0`
+SLIMERDIR=`dirname $0`
+SLIMERDIR=`cd $SLIMERDIR;pwd`
+CURRENTDIR=`pwd`
 
+cd $SLIMERDIR
 
-VERSION=`grep "^Version=" src/application.ini`
-VERSION=${VERSION:8}
-
-TARGETDIR="$CURRENTDIR/dist/slimerjs-$VERSION"
-
-if [ ! -d "$TARGETDIR" ]
+if [ "$1" == "nightly" ]
 then
+    VERSION="nightly"
+else
+    VERSION=`grep "^Version=" src/application.ini`
+    VERSION=${VERSION:8}
+fi
+
+TARGETDIR="$SLIMERDIR/_dist/slimerjs-$VERSION"
+
+if [ -d "$TARGETDIR" ]
+then
+    rm -rf $TARGETDIR/*
+else
     mkdir -p "$TARGETDIR"
 fi
 
+# copy files
 cp src/application.ini $TARGETDIR
 cp src/slimerjs $TARGETDIR
 cp src/slimerjs.bat $TARGETDIR
+cp README.md $TARGETDIR
+cp LICENSE $TARGETDIR
 cd src
-zip -r ../$TARGETDIR/omni.ja chrome/ components/ defaults/ modules/ chrome.manifest
-cd ..
-cd $CURRENTDIR/dist
+
+# set the build date
+BUILDDATE=`date +%Y%m%d`
+sed -i -e "s/BuildID=.*/BuildID=$BUILDDATE/g" application.ini
+
+# zip chrome files into omni.ja
+zip -r $TARGETDIR/omni.ja chrome/ components/ defaults/ modules/ chrome.manifest
+
+# create the final package
+cd $SLIMERDIR/_dist
 zip -r "slimerjs-$VERSION.zip" "slimerjs-$VERSION"
 
-echo ""
-echo "The package is here: dist/slimerjs-$VERSION.zip"
-
+cd $CURRENTDIR
+echo "The package is in $SLIMERDIR/_dist/slimerjs-$VERSION.zip"
