03aef659b42a2134e514bc5efef004a440611e34
fumitoito <weathercook+service@gmail.com>
2013-07-19 18:05:37 +0900

    env parameter should be rounded by double-quotation

--- src/slimerjs.bat
+++ src/slimerjs.bat
@@ -1,6 +1,6 @@
 @echo off
 
-SET SLIMERJSLAUNCHER=%SLIMERJSLAUNCHER%
+SET SLIMERJSLAUNCHER="%SLIMERJSLAUNCHER%"
 REM % ~ d[rive] p[ath] 0[script name] is the absolute path to this bat file, without quotes, always.
 REM ~ strips quotes from the argument
 SET SLIMERDIR=%~dp0
