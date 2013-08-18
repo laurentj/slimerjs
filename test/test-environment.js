4d8e54a5b5bc539d6bf02f635cd08d22b257d50a
Laurent Jouanneau <ljouanneau@gmail.com>
2013-08-18 10:49:26 +0200

    Bumped version to 0.8.3pre

--- test/test-environment.js
+++ test/test-environment.js
@@ -90,7 +90,7 @@ if ("slimer" in this) {
         it("should have a right version", function(){
             expect(slimer.version.major).toEqual(0);
             expect(slimer.version.minor).toEqual(8);
-            expect(slimer.version.patch).toEqual(2);
+            expect(slimer.version.patch).toEqual(3);
         });
 
     });
