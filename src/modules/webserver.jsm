laurentj/slimerjs

commit 7df55360bbd5c8c7d35ff70809a7b1138aea9ab2

Options parameter is recognized on webserver.listen but ignored
Our implementation of httpd.js does not support keep-alive connection.

src/modules/webserver.jsm
@@ -14,7 +14,15 @@ function create() {
             return "WebServer";
         },
 
-        listen: function(port, callback) {
+        /**
+         * @param integer|string port    port or "host:port"
+         * @param object opt            optional options. (not supported)
+         * @param function callback     optional callback
+         */
+        listen: function(port, opt, callback) {
+            if (arguments.length == 2 && "function" == typeof opt) {
+                callbac = opt;
+            }
             if (callback) {
                 this.registerPrefixHandler("/", callback);
             }

commit 2328644e21e8cdf760a59c7f5e0f5626449e9a47

Oups, fixed regression caused by commit 7df55360

src/modules/webserver.jsm
@@ -21,7 +21,7 @@ function create() {
          */
         listen: function(port, opt, callback) {
             if (arguments.length == 2 && "function" == typeof opt) {
-                callbac = opt;
+                callback = opt;
             }
             if (callback) {
                 this.registerPrefixHandler("/", callback);